4c92a952daa2f2f59966a185cebb138aa7b3f5a5
IT22309556 <it22309556@my.sliit.lk>
2026-09-24 19:31:20 +0530

    Add IT22309556Lab8Q2.java to Q2

---
+++ Q2/IT22309556Lab8Q2.java
@@ -0,0 +1,38 @@
+public class IT22309556Lab8Q2 {
+
+    public static void main(String[] args) {
+	
+        int[] A = {10, 20, 30, 40, 50};
+        int[] B = {34, 67, 12, 89, 12};
+        int[] C = new int[A.length];
+
+        // Print A Array Contents
+        System.out.println();
+        System.out.println("A Array Contents:");
+        for (int i = 0; i < A.length; i++) {
+            System.out.print(A[i] + " ");
+        }
+        System.out.println();
+
+        // Print B Array Contents
+        System.out.println();
+        System.out.println("B Array Contents:");
+        for (int i = 0; i < B.length; i++) {
+            System.out.print(B[i] + " ");
+        }
+        System.out.println();
+
+        // Calculate A + B and store in Array C
+        for (int i = 0; i < A.length; i++) {
+            C[i] = A[i] + B[i];
+        }
+
+        // Print C Array Contents (A + B)
+        System.out.println();
+        System.out.println("C Array Contents (A + B):");
+        for (int i = 0; i < C.length; i++) {
+            System.out.print(C[i] + " ");
+        }
+        System.out.println();
+    }
+}
